31c4bd8d5b7d7ec2c38ded31549c8c4852f2ad55
LopezRussiAlvaroFelipe <aflopez67@ucatolica.edu.co>
2022-09-28 23:33:02 -0500

    v 1.0.5

--- src/main/java/AvesTruz.java
+++ src/main/java/AvesTruz.java
@@ -3,16 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import org.sfl4j.Logger;
 /**
  *
  * @author pipel
  */
 public class AvesTruz extends Ave {
-
+   public static final Logger logger= LoggerFactory.getLogger(Avestruz.class)
    public Avestruz(){
-        tipoSonido= new SinCanto();
-        tipoVuelo= new SinVuelo();
+       
+       logger.debug("instanciaciando avestruz");
+       tipoSonido= new SinCanto();
+       tipoVuelo= new SinVuelo();
    }
 
 }
--- src/main/java/SinCanto.java
+++ src/main/java/SinCanto.java
@@ -3,14 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import org.sfl4j.Logger;
 /**
  *
  * @author pipel
  */
 public class SinCanto implements TipoSonido{
 
+    public static final Logger logger= LoggerFactory.getLogger(SinCanto.class)
     public void makeSound(){
+        logger.debug("<<Silencio>>");
         System.out.println("<<Silecio>>");
     }
     
