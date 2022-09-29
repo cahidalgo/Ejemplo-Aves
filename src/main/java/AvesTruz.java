e030773ea71725a2b8f721077838fa7fdc03b9f2
LopezRussiAlvaroFelipe <aflopez67@ucatolica.edu.co>
2022-09-28 23:05:11 -0500

    v 1.0.1

---
+++ src/main/java/AvesTruz.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pipel
+ */
+public class AvesTruz extends Ave {
+
+    @Override
+    public void makeSound() {
+        System.out.println("<<Silencio>>");
+        
+    }
+    
+    
+    
+}
